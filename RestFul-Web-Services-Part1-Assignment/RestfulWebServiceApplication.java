27a087c5efd06a48bb4e0c71862ec95b677baff2
harshyadav5 <harsh.yadav@tothenew.com>
2020-03-07 12:05:16 +0530

    RestFul-Web-Services-Part1-Assignment

---
+++ RestFul-Web-Services-Part1-Assignment/RestfulWebServiceApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.rest.webservice.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+
+}

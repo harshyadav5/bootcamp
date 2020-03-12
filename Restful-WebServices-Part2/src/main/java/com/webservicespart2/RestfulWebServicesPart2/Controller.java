6e26b4e914ec195d6bebc23c7e6d57975db56bac
harshyadav5 <harsh.yadav@tothenew.com>
2020-03-12 16:05:27 +0530

    Content Negotiation "Ques 3 and 4"

--- Restful-WebServices-Part2/src/main/java/com/webservicespart2/RestfulWebServicesPart2/Controller.java
+++ Restful-WebServices-Part2/src/main/java/com/webservicespart2/RestfulWebServicesPart2/Controller.java
@@ -3,15 +3,15 @@ package com.webservicespart2.RestfulWebServicesPart2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Locale;
 
 @RestController
 public class Controller{
+    @Autowired
+    private  EmployeeDaoServices employeeDaoServices;
     @Autowired
     private MessageSource messageSource;
 
@@ -22,6 +22,14 @@ public class Controller{
     @GetMapping("/helloWorld/{username}")
     public String returnHelloUserName(@PathVariable  String username){
         return "Hello"+username;
+    }
+    @GetMapping("/getEmployees")
+    public List<Employee> getAllEmployees(){
+        return employeeDaoServices.findAll();
+    }
+    @PostMapping("/addEmployee")
+    public void addNewEmployee(@RequestBody Employee emp){
+        Employee employee = employeeDaoServices.addEmployee(emp);
     }
 }
     

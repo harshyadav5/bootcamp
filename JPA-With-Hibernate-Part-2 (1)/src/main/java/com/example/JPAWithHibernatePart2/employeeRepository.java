2b4dbfb694886398a099fdadc9cdfc8c5f9a061c
harshyadav5 <harsh.yadav@tothenew.com>
2020-03-18 15:27:57 +0530

    JPQL

---
+++ JPA-With-Hibernate-Part-2 (1)/src/main/java/com/example/JPAWithHibernatePart2/employeeRepository.java
@@ -0,0 +1,31 @@
+package com.example.JPAWithHibernatePart2;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface employeeRepository extends CrudRepository<Employee,Integer> {
+    @Query("select firstName, lastName from Employee where salary >(select AVG(salary) from Employee)")
+    List<Object[]> findfirstNameAndlastName(PageRequest pageable);
+
+    @Query("select AVG(salary) from Employee")
+    Integer findAvarageSalary();
+
+    @Transactional
+    @Query("update Employee set salary=:salarypassed where salary <:avg")
+    @Modifying
+    void updateSalary(@Param("avg") Integer avgsalary,@Param("salarypassed") Integer amount);
+
+    @Query("select MIN(salary) from Employee")
+    Integer findMinSalary();
+
+    @Transactional
+    @Query("delete from Employee where salary=:minsalary")
+    @Modifying
+    void deleteEmployee(@Param("minsalary") Integer salary);
+}
---
+++ JPA-With-Hibernate-Part-2 (1)/src/test/java/com/example/JPAWithHibernatePart2/JpaWithHibernatePart2ApplicationTests.java
@@ -0,0 +1,92 @@
+package com.example.JPAWithHibernatePart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+@SpringBootTest
+class JpaWithHibernatePart2ApplicationTests {
+	@Autowired
+	employeeRepository repository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee() {
+		Employee employee = new Employee();
+		employee.setId(4040);
+		employee.setFirstName("Kshitiz");
+		employee.setLastName("Gupta");
+		employee.setSalary(22750);
+		employee.setAge(22);
+		repository.save(employee);
+		System.out.println("Value Updated==================");
+	}
+	@Test
+	public void findAllEmployee(){
+		Pageable pageable = PageRequest.of(0,2);
+		List<Employee> employees = (List<Employee>) repository.findAll();
+		employees.forEach(e-> System.out.println(e));
+
+	}
+
+	//Ques 1: Display the first name, last name of all employees having salary greater than average salary ordered in ascending by their age and in descending by their salary.
+	@Test
+	public void findfirstNameAndlastName() {
+		Sort sort = Sort.by(Sort.Order.asc("age"),Sort.Order.desc("salary"));
+		Pageable pageable = PageRequest.of(0,2,sort);
+		List<Object[]> employees = repository.findfirstNameAndlastName((PageRequest) pageable);
+		for (Object[] objects : employees) {
+			System.out.println(objects[0]+" ");
+			System.out.println(objects[1]+" ");
+		}
+	}
+
+	//Output:
+	// +--------+---------------+--------------+------------+---------+
+	//| emp_id | emp_firstname | emp_lastname | emp_salary | emp_age |
+	//+--------+---------------+--------------+------------+---------+
+	//|   4040 | Kshitiz       | Gupta        |      22750 |      22 |
+	//|   4041 | Harsh         | Yadav        |      15000 |      21 |
+	//+--------+---------------+--------------+------------+---------+
+
+	//Ques 2:Update salary of all employees by a salary passed as a parameter whose existing salary is less than the average salary.
+	@Test
+	@Rollback(value = false)
+	public void updateSalary(){
+		Integer agvsalary = repository.findAvarageSalary();
+		repository.updateSalary(agvsalary,12000);
+	}
+	//Output:
+	// +--------+---------------+--------------+------------+---------+
+	//| emp_id | emp_firstname | emp_lastname | emp_salary | emp_age |
+	//+--------+---------------+--------------+------------+---------+
+	//|   4040 | Kshitiz       | Gupta        |      22750 |      22 |
+	//|   4041 | Harsh         | Yadav        |      12000 |      21 |
+	//+--------+---------------+--------------+------------+---------+
+
+	//Ques 3:Delete all employees with minimum salary.
+	@Test
+	@Rollback(value = false)
+	public void delete(){
+		Integer result = repository.findMinSalary();
+		repository.deleteEmployee(result);
+	}
+	//Output:
+	// +--------+---------------+--------------+------------+---------+
+	//| emp_id | emp_firstname | emp_lastname | emp_salary | emp_age |
+	//+--------+---------------+--------------+------------+---------+
+	//|   4040 | Kshitiz       | Gupta        |      22750 |      22 |
+	//+--------+---------------+--------------+------------+---------+
+
+
+}
+

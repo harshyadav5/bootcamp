cb1150b7a29a9553cbc401a0a869ebefc716ae67
harshyadav5 <harsh.yadav@tothenew.com>
2020-03-18 15:15:56 +0530

    JPQL

---
+++ JPA-With-Hibernate-Part-2 (1)/src/main/java/com/example/JPAWithHibernatePart2/JpaWithHibernatePart2Application.java
@@ -0,0 +1,13 @@
+package com.example.JPAWithHibernatePart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWithHibernatePart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWithHibernatePart2Application.class, args);
+	}
+
+}

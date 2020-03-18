b6ce7ff68ebb87d2b42bf57967ea60a8f5e15956
harshyadav5 <harsh.yadav@tothenew.com>
2020-03-18 15:22:02 +0530

    removing files to upload later using .gitignore

--- Spring-Data-JPA-with-Hibernate-Part-1/src/main/java/SpringDataJpa/JPAAssignment/EmployeeRepository/EmployeePagingAndSortingRepository.java
+++
@@ -1,7 +0,0 @@
-package SpringDataJpa.JPAAssignment.EmployeeRepository;
-
-import SpringDataJpa.JPAAssignment.Employee;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EmployeePagingAndSortingRepository extends PagingAndSortingRepository<Employee,Integer> {
-}

1af012fc061d9175ed36f6b92009eee93c58e6f8
harshyadav5 <harsh.yadav@tothenew.com>
2020-05-06 01:01:57 +0530

    Burger-Builder

---
+++ Burger-Builder/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    </div>
+  );
+}
+
+export default App;
